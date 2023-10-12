c75b5fb920dfba565291072f8cad6f1ba652aa2a
Alexandre Moreira <alexandremoreira@Macs-MacBook-Pro.local>
2023-10-12 11:15:31 -0300

    condicional ternaria

---
+++ controle-de-fluxo/SituacaoEscolar.java
@@ -0,0 +1,10 @@
+public class SituacaoEscolar {
+    public static void main(String[] args) {
+        int nota = 6;
+
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+
+        System.out.println(resultado);
+    }
+    
+}
